1e418d467f4b819b85143e3cb2f688b3b5f0d27e
geoffroy moreaud <g.moreaud@gmail.com>
2023-03-25 22:59:58 +0100

    all good sauf le delete

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,11 +13,28 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-    if @song
+    if @song.save
       redirect_to song_path(@song)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params)
+    # No need for app/views/restaurants/update.html.erb
+    redirect_to song_path(@song)
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy!
+    redirect_to songs_path, status: :see_other
   end
 
   private
